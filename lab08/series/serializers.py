9250fa12a769cce936abf679f7709057003fc32f
anderact <axel.chacon@tecsup.edu.pe>
2022-10-30 13:24:56 -0500

    first commit

---
+++ lab08/series/serializers.py
@@ -0,0 +1,8 @@
+from platform import release
+from rest_framework import serializers
+from .models import Serie
+
+class SerieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Serie
+        fields = ('id', 'name', 'release_date', 'rating', 'category')
